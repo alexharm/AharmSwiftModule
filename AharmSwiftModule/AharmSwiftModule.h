790049d441b2e9f8e80cbece7c0165c8fdf906a8
alexharm <alexharm@gmail.com>
2021-12-07 16:46:09 -0700

    Initial Commit

---
+++ AharmSwiftModule/AharmSwiftModule.h
@@ -0,0 +1,18 @@
+//
+//  AharmSwiftModule.h
+//  AharmSwiftModule
+//
+//  Created by Alexander Harm on 12/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AharmSwiftModule.
+FOUNDATION_EXPORT double AharmSwiftModuleVersionNumber;
+
+//! Project version string for AharmSwiftModule.
+FOUNDATION_EXPORT const unsigned char AharmSwiftModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AharmSwiftModule/PublicHeader.h>
+
+
