b0ae33c4d0f0ff5ba992d901fa085f1fa767caa8
alexharm <alexharm@gmail.com>
2021-12-07 17:23:17 -0700

    committed

---
+++ AharmSharedModule/TestAgain.swift
@@ -0,0 +1,13 @@
+//
+//  TestAgain.swift
+//  AharmSharedModule
+//
+//  Created by Alexander Harm on 12/7/21.
+//
+
+import Foundation
+
+public class TestAgain
+{
+    public static let TEST_CONSTANT_A = "A";
+}
